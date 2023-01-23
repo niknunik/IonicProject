914cae6e7ea7f017697d5cd33b67a52373410bdd
nunik <nunik06@gmail.com>
2023-01-23 10:51:04 +0700

    add product

---
+++ src/app/services/products/products.service.ts
@@ -0,0 +1,261 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  selectedCategories: any = [];
+
+  priceRange: PriceRange = {
+    lower: 0,
+    upper: 100,
+    applied: false
+  };
+
+  allProducts: any = [];
+  cartCount: number = 0;
+
+  show_result_size: boolean = true;
+
+  bannerImages = [
+    {
+      imgurl: 'assets/images/slide1.jpg'
+    }, {
+      imgurl: 'assets/images/slide2.jpg'
+    }, {
+      imgurl: 'assets/images/slide3.jpg'
+    }
+  ];
+
+  products = [
+    {
+      id : 1,
+      imgurl: 'assets/images/converse.jpg',
+      name: 'Convers Classic',
+      category: 'Shoes',
+      price: 67,
+      icon: 'heart',
+      totalStock: 10
+    
+    }, {
+      id : 2,
+      imgurl: 'assets/images/sunflower1.jpg',
+      name: 'Convers Sunflower',
+      category: 'Shoes',
+      price: 37,
+      icon: 'heart',
+      totalStock: 10
+    }, {
+      id : 3,
+      imgurl: 'assets/images/bag.jpg',
+      name: 'White-Bag',
+      category: 'Bag',
+      price: 77,
+      icon: 'heart',
+      totalStock: 10
+     
+    }, {
+      id : 4,
+      imgurl: 'assets/images/t-shirt.jpg',
+      name: 'Tee Cutton',
+      category: 'T-Shirt',
+      price: 65,
+      icon: 'heart',
+      totalStock: 10
+    }
+  ];
+
+  categories : any = [
+    {
+      category : 'Shoes'
+    }, {
+      category : 'Bag'
+    }, {
+      category : 'T-Shirt'
+    }
+  ];
+
+  sort: Sort = {
+    latest: false,
+    price_lth: false,
+    price_htl: false
+  };
+
+  beforeSort : any;
+
+  constructor(
+  ) { 
+    
+  }
+
+  initProductList() {
+    this.allProducts = this.products;
+    this.showResultCount();
+  }
+
+  searchProducts( term: string ) {
+    this.products = [];
+    if ( `${term}`.trim() ) {
+      let NotFoundInName = this.allProducts.map(item => {
+        if ( item.name.toLocaleLowerCase().indexOf(term.toLocaleLowerCase()) < 0 ) {
+          return item;
+        } else {
+          this.products.push(item);
+        }
+      });
+      console.log({NotFoundInName});
+
+      let NotFoundInCategory = NotFoundInName.map(item => {
+        if (item) {
+          if ( item.category.toLocaleLowerCase().indexOf(term.toLocaleLowerCase()) < 0 ) {
+            return item;
+          } else {
+            this.products.push(item);
+          }
+        }
+      });
+      console.log({NotFoundInCategory});
+
+      let foundInPrice = NotFoundInCategory.map(item => {
+        if (item) {
+          if ( `${item.price}`.toLocaleLowerCase().indexOf(term.toLocaleLowerCase()) > -1 ) {
+            this.products.push(item);
+          }
+        }
+      });
+    } else {
+      this.products = this.allProducts;
+    }
+
+    this.showResultCount();
+  }
+
+  applyLocalSort ( column, order, type )  {
+    this.uncheckSorts();
+    this.sort[type] = true;
+    console.log('column :>> ', column);
+    this.beforeSort = this.products;
+
+    this.products = this.beforeSort.sort((a, b) => {
+      console.log('sort :>> ', a, b);
+      if ( order === 'desc' ) {
+        return a[column] > b[column];
+      } else {
+        return a[column] < b[column];
+      }
+    });
+
+    this.showResultCount();
+  }
+
+  applyFilter() {
+    console.log(this.selectedCategories, this.priceRange);
+    if ( this.selectedCategories.length > 0 || this.priceRange.applied ) {
+      console.log('Filter applied :>> ');
+      this.products = [];
+
+      for(let i = 0; i < this.allProducts.length; i++) {
+        let foundCategory = true, foundPrice = true;
+
+        if ( this.selectedCategories.length > 0 ) {
+          foundCategory = this.selectedCategories.some( val => val.category.toLocaleLowerCase() === this.allProducts[i]['category'].toLocaleLowerCase() && val.isChecked);
+        }
+        
+        if ( this.priceRange.applied ) {
+          let price = this.allProducts[i]['price'];
+          foundPrice = ( price >= this.priceRange.lower && price <= this.priceRange.upper );
+        }
+        
+        if(foundCategory && foundPrice) {
+          this.products.push(this.allProducts[i]);
+        }
+        
+      }
+    } else {
+      console.log('No Filter found:>> ');
+      this.products = this.allProducts;
+    }
+
+    if ( Object.values(this.sort).some(el => el) ) {
+      this.verifySort();
+    }
+
+    this.showResultCount();
+  }
+  
+  verifySort() {
+    if ( this.sort.latest ) {
+      this.applyLocalSort ( 'id', 'asc', 'latest');
+    } else if (this.sort.price_lth) {
+      this.applyLocalSort ( 'price', 'desc', 'price_lth' );
+    } else if (this.sort.price_htl) {
+      this.applyLocalSort ( 'price', 'asc', 'price_htl' );
+    }
+  }
+
+
+  showResultCount() {
+    this.show_result_size = true;
+    setTimeout(() => {
+      this.show_result_size = false;
+    }, 2000);
+  }
+
+  resetItems() {
+    this.products = [];
+    this.allProducts = [];
+    this.beforeSort = [];
+
+    this.uncheckFilters();
+  } 
+
+  uncheckFilters() {
+    this.selectedCategories = []
+    this.defaultPriceRange();
+
+    for( let i = 0; i < this.categories.length; i++) {
+      this.categories[i].isChecked = false;
+    }
+  }
+
+  defaultPriceRange() {
+    this.priceRange = {
+      applied:  false,
+      lower : 0,
+      upper : 100
+    }
+    
+  }
+
+  uncheckSorts() {
+    this.defaultSorting();
+  }
+
+  defaultSorting() {
+    Object.keys(this.sort).forEach(key => {
+      this.sort[key] = false;
+    })
+  }
+
+  isSorted() {
+    return Object.values(this.sort).some(el => el);
+  }
+
+  isFiltered() {
+    return this.selectedCategories.length > 0 || this.priceRange.applied;
+  }
+
+}
+
+interface PriceRange {
+  lower: any,
+  upper: any,
+  applied: boolean
+}
+
+interface Sort {
+  latest: boolean,
+  price_lth: boolean,
+  price_htl: boolean
+}
---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,132 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActionSheetController, IonRouterOutlet, ModalController } from '@ionic/angular';
+import { AddToCartPage } from '../pages/add-to-cart/add-to-cart.page';
+import { ProductFilterPage } from '../pages/product-filter/product-filter.page';
+import { CartService } from '../services/cart/cart.service';
+import { ProductsService } from '../services/products/products.service';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  // set app banner slides
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    }
+  };
+
+  bannerImages: any = [];
+  //searchTerm: string;
+
+  constructor(
+    public productService : ProductsService,
+    public routerOutlet : IonRouterOutlet,
+    public modalCtrl : ModalController,
+    public cart : CartService,
+    private router: Router,
+    private actionSheetController: ActionSheetController
+  ) {
+    this.bannerImages = this.productService.bannerImages;
+    this.productService.initProductList();
+  }
+
+  search(event: any) {
+    let term = event.target.value;
+    console.log({term});
+    this.productService.searchProducts(term);
+  }
+
+  async addToCartModal(item) {
+    console.log('item_id :>> ', item);
+    let isAdded = this.cart.isAddedToCart(item.id);
+
+    if ( !isAdded ) {
+      this.cart.placeItem(item);
+      const modal = await this.modalCtrl.create({
+        component: AddToCartPage,
+        cssClass: 'add-to-cart-modal',
+        presentingElement: this.routerOutlet.nativeEl
+      });
+  
+      await modal.present();
+      
+      await modal.onWillDismiss().then((result) => {
+        console.log('result :>> ', result);
+      }).catch((err) => {
+        console.log('err :>> ', err);
+      });
+
+    } else {
+      this.router.navigate(['/tabs/tab2']);
+    }
+    
+  }
+
+  async filterPage() {
+    const modal = await this.modalCtrl.create({
+      component: ProductFilterPage,
+      //cssClass: ,
+      presentingElement: this.routerOutlet.nativeEl
+    });
+
+    await modal.present();
+    
+    await modal.onWillDismiss().then((result) => {
+      console.log('result :>> ', result);
+    }).catch((err) => {
+      console.log('err :>> ', err);
+    });
+  }
+
+  async sortProducts() {
+    console.log('productService.sort :>> ', this.productService.sort);
+    const actionSheet = await this.actionSheetController.create({
+        header: "Sort by",
+        mode: "ios",
+        cssClass: "sort-products",
+        buttons: [{
+                text: 'Latest Products',
+                role: this.productService.sort.latest ? 'selected' : '',
+                handler: () => {
+                    this.productService.applyLocalSort ( 'id', 'asc', 'latest');
+                }
+            },
+            {
+                text: 'Price - Low to High',
+                role: this.productService.sort.price_lth ? 'selected' : '',
+                handler: () => {
+                  this.productService.applyLocalSort ( 'price', 'desc', 'price_lth' );
+                }
+            },
+            {
+                text: 'Price - High to Low',
+                role: this.productService.sort.price_htl ? 'selected' : '',
+                handler: () => {
+                  this.productService.applyLocalSort ( 'price', 'asc', 'price_htl' );
+                }
+                
+            },
+            {
+              text: 'Cancel',
+              icon: 'close',
+              role: 'cancel',
+              handler: () => {
+                console.log('Cancel clicked');
+              }
+            }
+        ]
+    });
+    await actionSheet.present();
+  }
+
+
+}
